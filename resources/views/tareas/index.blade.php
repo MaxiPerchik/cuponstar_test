ec326402d4afed99b0556d38b3695da99316acf5
MaxiPerchik <mperchik@protonmail.com>
2022-11-16 18:51:15 -0300

    modificacion de tareas.index

--- resources/views/tareas/index.blade.php
+++ resources/views/tareas/index.blade.php
@@ -9,7 +9,36 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    You're logged in!
+                    <a href="{{ route('tareas.create') }}">Agregar una Tarea Nueva</a>
+                    <br><br>
+                    <table>
+                        <thead>
+                        <tr>
+                            <th>Tareas</th>
+                            <th></th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach($tareas as $tarea)
+                            <tr>
+                                <td>{{ $tarea->titulo }}</td>
+                                <td>{{ $tarea->descripcion }}</td>
+                                <td class="flex">
+                                    <a class="inline-flex items-center ml-4 px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150"
+                                       href="{{ route('tareas.edit', $tarea) }}"> Editar</a>
+                                    <form method="POST" action="{{ route('tareas.destroy',$tarea) }}">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button
+                                            class="inline-flex items-center ml-4 px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150"
+                                            type="submit" onclick="return confirm('Estas seguro?')">Eliminar
+                                        </button>
+                                    </form>
+                                </td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
