09204a3b5d037391618f33bf0ed98fccce838da0
MaxiPerchik <mperchik@protonmail.com>
2022-11-16 17:35:28 -0300

    cambio de botones al espanol y creamos en la DB un estado de prueba para visualizar

--- resources/views/estados/index.blade.php
+++ resources/views/estados/index.blade.php
@@ -24,13 +24,13 @@
                                 <td>{{ $estado->estado }}</td>
                                 <td class="flex">
                                     <a class="inline-flex items-center ml-4 px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150"
-                                       href="{{ route('estados.edit', $estado) }}"> Edit</a>
-                                    <form method="POST" action="{{ route('categories.destroy',$estado) }}">
+                                       href="{{ route('estados.edit', $estado) }}"> Editar</a>
+                                    <form method="POST" action="{{ route('estados.destroy',$estado) }}">
                                         @csrf
                                         @method('DELETE')
                                         <button
                                             class="inline-flex items-center ml-4 px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150"
-                                            type="submit" onclick="return confirm('Are you sure?')">Delete
+                                            type="submit" onclick="return confirm('Are you sure?')">Eliminar
                                         </button>
                                     </form>
                                 </td>
