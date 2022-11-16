5499d4a7745176ea75acee4ee3adc1e8a78b629e
MaxiPerchik <mperchik@protonmail.com>
2022-11-16 18:00:31 -0300

    trate de probar el edit y me di cuenta de que me olvide de modificar los Models para que sean fillable en los campos que queria editar

--- resources/views/estados/edit.blade.php
+++ resources/views/estados/edit.blade.php
@@ -12,9 +12,9 @@
                     <form method="POST" action="{{ route('estados.update',$estado) }}">
                         @csrf
                         @method('PUT')
-                        Name:
+                        Estado:
                         <br>
-                        <input class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm" type="text"  name="name" value="{{ $estado->estado }}">
+                        <input class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm" type="text"  name="estado" value="{{ $estado->estado }}">
                         <br><br>
                         <button
                             class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150"
