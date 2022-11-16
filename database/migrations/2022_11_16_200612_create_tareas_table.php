318e34701f2122fb467b1667a54a9c65abe02163
MaxiPerchik <mperchik@protonmail.com>
2022-11-16 17:09:49 -0300

    creamos los camposd e las tablas y migramos para crearlas en la BD

--- database/migrations/2022_11_16_200612_create_tareas_table.php
+++ database/migrations/2022_11_16_200612_create_tareas_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('tareas', function (Blueprint $table) {
             $table->id();
+            $table->string('titulo');
+            $table->string('descripcion');
+            $table->foreignId('estado_id')->constrained();
             $table->timestamps();
         });
     }
